7d407b262daf0373f25e855a6b9ee08e9df5bcfb
Jean0405 <jeanpierrea0405@gmail.com>
2024-01-17 15:58:23 -0500

    feat: :sparkles: aos animation added

--- src/app/pages/visits/page.jsx
+++ src/app/pages/visits/page.jsx
@@ -1,4 +1,10 @@
+"use client"
 import "./index.css";
+
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 
 import Image from "next/image";
 import Link from "next/link";
@@ -19,24 +25,29 @@ import v_slider_img10 from "../../../../public/img/v_slider_img10.png";
 
 
 function SliderInfinite() {
+
+  useEffect(() => {
+    AOS.init({ duration: 900 });
+  });
+
   return (
     <div className="relative slider-container">
       <div className='vertical_slider hidden md:flex justify-center gap-3'>
-        <div className="slider_track gap-3">
+        <div className="slider_track gap-3" >
           <div className="slide"><Image className="slide_img" src={v_slider_img9} /></div>
           <div className="slide"><Image className="slide_img" src={v_slider_img8} /></div>
           <div className="slide"><Image className="slide_img" src={v_slider_img2} /></div>
           <div className="slide"><Image className="slide_img" src={v_slider_img7} /></div>
           <div className="slide"><Image className="slide_img" src={v_slider_img6} /></div>
         </div>
-        <div className="slider_track middle gap-3">
+        <div className="slider_track middle gap-3" >
           <div className="slide"><Image className="slide_img" src={v_slider_img6} /></div>
           <div className="slide"><Image className="slide_img" src={v_slider_img10} /></div>
           <div className="slide"><div className="slide_img text-center text-2xl font-bold">CONOCE<br />NUESTROS<br />ESPACIOS</div></div>
           <div className="slide"><Image className="slide_img" src={v_slider_img4} /></div>
           <div className="slide"><Image className="slide_img" src={v_slider_img8} /></div>
         </div>
-        <div className="slider_track gap-3">
+        <div className="slider_track gap-3" >
           <div className="slide"><Image className="slide_img" src={v_slider_img6} /></div>
           <div className="slide"><Image className="slide_img" src={v_slider_img1} /></div>
           <div className="slide"><Image className="slide_img" src={v_slider_img3} /></div>
@@ -44,7 +55,7 @@ function SliderInfinite() {
           <div className="slide"><Image className="slide_img" src={v_slider_img5} /></div>
         </div>
       </div>
-      <button as={Link} className="explore_link">Explorar <FontAwesomeIcon className="icon" icon={faArrowRight} width={16} /></button>
+      <Link href="/pages/meetUs" className="explore_link" data-aos="zoom-in">Explorar <FontAwesomeIcon className="icon" icon={faArrowRight} width={16} /></Link>
     </div>
   )
 }
