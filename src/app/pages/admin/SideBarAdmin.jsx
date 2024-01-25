1207f29aebe0104979e0443f96fca10a9628542a
Jean0405 <jeanpierrea0405@gmail.com>
2024-01-25 16:52:42 -0500

    feat: :construction: sidebar admin in cprocess

--- src/app/pages/admin/SideBarAdmin.jsx
+++ src/app/pages/admin/SideBarAdmin.jsx
@@ -1,35 +1,57 @@
 import React from "react";
-import { faRectangleList } from "@fortawesome/free-solid-svg-icons";
+import {
+  faRectangleList,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import campusLogo from "../../../../public/img/logoCampus.svg";
 import Image from "next/image";
 import { Divider } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
+import { logOut } from "@/utils/auth";
+import { checkResponseStatus } from "@/helpers/checkResponses";
+import { showErrorToast } from "@/helpers/Toasts";
 
 export default function SideBarAdmin() {
+   const router = useRouter();
+
+   const handleLogOut = async () => {
+      const token = localStorage.getItem("token");
+      let response = await logOut(token);
+  
+      response = checkResponseStatus(response, 200);
+  
+      if (!response) {
+        showErrorToast();
+      }
+      localStorage.removeItem("token");
+      router.push("/pages/login");
+    };
+
   return (
     <>
-      <div className="relative flex-col p-2 bg-neutral-200 fixed">
-         <div>
-            <Image className="w-16" src={campusLogo} alt="Logo Campus"/>
-         </div>
-         <Divider className="my-5"/>
-        <div className="flex flex-col justify-start items-center gap-5">
-          <div className="cursor-pointer text-2xl text-neutral-600">
-            <FontAwesomeIcon icon={faRectangleList}/>
-          </div>
-          <div className="cursor-pointer text-2xl text-neutral-600">
+      <div className="relative hidden sm:flex flex-col pt-5 px-2">
+        <div>
+          <Image className="w-16" src={campusLogo} alt="Logo Campus" />
+        </div>
+        <Divider className="my-5" />
+        <div className="flex flex-col justify-center items-center gap-5">
+          <div className="cursor-pointer text-2xl text-neutral-500 hover:text-[#F4B422]">
             <FontAwesomeIcon icon={faRectangleList} />
           </div>
-          <div className="cursor-pointer text-2xl text-neutral-600">
+          <div className="cursor-pointer text-2xl text-neutral-500 hover:text-[#F4B422]">
             <FontAwesomeIcon icon={faRectangleList} />
           </div>
-          <div className="cursor-pointer text-2xl text-neutral-600">
+          <div className="cursor-pointer text-2xl text-neutral-500 hover:text-[#F4B422]">
+            <FontAwesomeIcon icon={faRectangleList} />
+          </div>
+          <div className="cursor-pointer text-2xl text-neutral-500 hover:text-[#F4B422]">
             <FontAwesomeIcon icon={faRectangleList} />
           </div>
         </div>
-        <div className="flex justify-center items-center text-neutral-600 text-2xl" style={{ marginTop: 'auto' }}>
-         <FontAwesomeIcon icon={faRectangleList}/>
+        <div className="absolute bottom-2 left-6 cursor-pointer text-2xl text-[#F4B422] hover:text-neutral-700">
+          <FontAwesomeIcon icon={faRightFromBracket} onClick={()=>handleLogOut()}/>
         </div>
       </div>
     </>
