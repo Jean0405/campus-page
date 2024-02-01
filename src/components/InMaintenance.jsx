73007f7102a4dbaf1fb4545e016f8927b50d8aa9
Jean0405 <jeanpierrea0405@gmail.com>
2024-02-01 07:49:07 -0500

    style: :lipstick: in maintenance page fixed

--- src/components/InMaintenance.jsx
+++ src/components/InMaintenance.jsx
@@ -11,7 +11,7 @@ function InMaintenance() {
     <div className='relative h-screen flex flex-col justify-center items-center gap-3 text-center overflow-hidden bg-emerald-50'>
         <h1 className='font-bold text-xl md:text-2xl lg:text-4xl uppercase'>Esta <span className='text-[#00AA80]'>página</span> está actualmente en mantenimiento</h1>
         <p className='text-lg md:text-2xl'>Estaremos de vuelta pronto, <span className='text-[#00AA80]'>gracias</span> por tu paciencia</p>
-        <Button as={Link} href="/" className="bg-[#00AA80] font-bold text-white">Volver</Button>
+        <Button as={Link} href="/" className="bg-[#00AA80] font-bold text-white z-10">Volver</Button>
         <Image className='absolute bottom-0 md:right-10' width={500} src={Astronaut} alt='Astronaut in maintenance logo'/>
     </div>
   )
